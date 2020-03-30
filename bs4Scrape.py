1fb07d47e89e7718ba8342bfabd8c4a5844dc66c
Bridget Asser <bridgetasser@gmail.com>
2020-03-30 12:38:34 -0400

    crawling and scraping next pages

--- bs4Scrape.py
+++ bs4Scrape.py
@@ -5,33 +5,35 @@ import requests
 url = "https://boston.craigslist.org/search/sof"
 
 
-response = requests.get(url)
+job_no = 0
+while True:
+
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data,'html.parser')
+    jobs = soup.find_all('p',{'class':'result-info'})
 
 
-#print(response)
+    for job in jobs:
+        title = job.find('a',{'class':'result-title'}).text
+        location_tag = job.find('span',{'class':'result-hood'})
+        location = location_tag.text[2:-1] if location_tag else "N/A"
+        date = job.find('time',{'class':'result-date'}).text
+        link = job.find('a',{'class':'result-title'}).get('href')
+        job_response = requests.get(link)
+        job_data = job_response.text
+        job_soup = BeautifulSoup(job_data, 'html.parser')
+        job_description = job_soup.find('section',{'id':'postingbody'}).text
+        job_attributes_tag = job_soup.find('p',{'class':'attrgroup'})
+        job_attributes = job_attributes_tag.text if job_attributes_tag else "N/A"
+        print('Job Title:', title, '\nLocation', location, '\nDate:', date, '\nLink:', link, "\n", job_attributes, '\nJob Description:', job_description,'\n---')
+    url_tag = soup.find('a',{'title':'next page'})
+    if url_tag.get('href'):
+        url= 'https://boston.craigslist.org' + url_tag.get('href')
+        print(url)
+    else:
+        break
+        
+print("Total Jobs:", job_no)
 
 
-data = response.text
-
-
-soup = BeautifulSoup(data,'html.parser')
-
-
-jobs = soup.find_all('p',{'class':'result-info'})
-
-
-for job in jobs:
-    title = job.find('a',{'class':'result-title'}).text
-    location_tag = job.find('span',{'class':'result-hood'})
-    location = location_tag.text[2:-1] if location_tag else "N/A"
-    date = job.find('time',{'class':'result-date'}).text
-    link = job.find('a',{'class':'result-title'}).get('href')
-    job_response = requests.get(link)
-    job_data = job_response.text
-    job_soup = BeautifulSoup(job_data, 'html.parser')
-    job_description = job_soup.find('section',{'id':'postingbody'}).text
-    job_attributes_tag = job_soup.find('p',{'class':'attrgroup'})
-    job_attributes = job_attributes_tag.text if job_attributes_tag else "N/A"
-
-
-    print('Job Title:', title, '\nLocation', location, '\nDate:', date, '\nLink:', link, "\n", job_attributes, '\nJob Description:', job_description,'\n---')
